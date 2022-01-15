f12cee5d83cd1e357410a07e20f6143b7665ef7e
al20vortex <al20vortex@gmail.com>
2022-01-15 13:37:11 -0800

    changed some files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/login.js'
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import axios from 'axios'
+import React from 'react';
+
+// woo! 
+let variableToShow = 'Not okay';
+
+// https://stackoverflow.com/questions/43493373/react-component-not-updating-after-changing-a-value
+// to fix the variableToShow
+
+async function getExample() {
+  try {
+    const response = await axios(
+      {
+        url: 'http://localhost:5012/info',
+        method: 'get',
+        headers: { 'Content-Type': 'application-json' },
+      }
+    );
+    variableToShow = 'Okay!';
+    return response;
+  } catch {
+    console.log('Something went wrong')
+  }
+}
 
 function App() {
+  getExample().then(() => {
+    console.log('getExample ran!')
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Happy happy happy
+      {variableToShow}
+      <Login/>
     </div>
   );
 }
 
-export default App;
+export default App
